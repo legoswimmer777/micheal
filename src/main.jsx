29bbbaf62aeeb2b95bb327815b9dd75416d66027
legoswimmer777 <llooggaann747@gmail.com>
2026-05-19 13:57:36 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-function App() {
-  return (
-    <div className="container">
-      {/* Background Glow Effects */}
-      <div className="glow glow1"></div>
-      <div className="glow glow2"></div>
-      <div className="glow glow3"></div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-      {/* Animated Number */}
-      <h1 className="logo">67</h1>
-
-      {/* Subtitle */}
-      <p className="subtitle">Mango Energy</p>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
